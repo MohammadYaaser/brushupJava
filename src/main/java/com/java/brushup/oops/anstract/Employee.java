f9c0d6ced0466faf2ca6a9f27453a5ec70e5d4af
MohammadYaaser <MohammadYaseer@.github.com>
2020-06-20 14:30:58 +0530

    Abstraction completed

--- src/main/java/com/java/brushup/oops/anstract/Employee.java
+++ src/main/java/com/java/brushup/oops/anstract/Employee.java
@@ -3,7 +3,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Employee extends Person {
 
-    private static final AtomicInteger ID_GENERATOR = new AtomicInteger(100);
     private boolean employee;
 
     public Employee(int empId, String name, String gender, String email, String phone, String country, boolean working) {
@@ -21,20 +20,5 @@ public class Employee extends Person {
         }
     }
 
-    public static void main(String[] args) {
-        Person student = new Employee(ID_GENERATOR.getAndIncrement(), "Mohammad Yaseer", "Male", "mohammadyaseer@icloud.com",
-                "9731878897", "India", false);
-        student.work();
-        student.setName("Yaseer Hussain");
 
-        System.out.println(student.toString());
-
-        Person teacher = new Employee(ID_GENERATOR.getAndIncrement(), "Johny Depp", "Male", "Johny.depp@icloud.com"
-                , "No Set", "USA", true);
-
-        teacher.work();
-        teacher.setName("Captain Jack Sparrow");
-        System.out.println(teacher.toString());
-
-    }
 }
---
+++ src/main/java/com/java/brushup/oops/anstract/Main.java
@@ -0,0 +1,24 @@
+package com.java.brushup.oops.anstract;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    private static final AtomicInteger ID_GENERATOR = new AtomicInteger(100);
+
+    public static void main(String[] args) {
+        Person student = new Employee(ID_GENERATOR.getAndIncrement(), "Mohammad Yaseer", "Male", "mohammadyaseer@icloud.com",
+                "9731878897", "India", false);
+        student.work();
+        student.setName("Yaseer Hussain");
+
+        System.out.println(student.toString());
+
+        Person teacher = new Employee(ID_GENERATOR.getAndIncrement(), "Johny Depp", "Male", "Johny.depp@icloud.com"
+                , "No Set", "USA", true);
+
+        teacher.work();
+        teacher.setName("Captain Jack Sparrow");
+        System.out.println(teacher.toString());
+
+    }
+}
